0baf8854c2b7a19d36f83577cb181adb40923612
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-16 22:00:49 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    tempArr = []
+    arguments.each do |argument|
+      self.each do |key, value|
+        if value == argument
+          tempArr.push(key)
+        end 
+      end 
+    end
+    
+    return tempArr
   end
 end
