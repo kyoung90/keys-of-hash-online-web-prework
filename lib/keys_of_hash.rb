a2c4cef321e93bc6572e19863488e6cb82fddf4c
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-16 21:50:42 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,9 +1,10 @@
 class Hash
   def keys_of(*arguments)
     # code goes here
+    tempArr = []
     arguments.each do |argument|
       self.each do |key, value|
-        
+        if 
       end
     end 
   end
